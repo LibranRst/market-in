16f08af8daad5cd6652c9f3817d8a57cfcd63e90
Libran Dev. <mlibran.hyuzin@protonmail.com>
2024-03-17 12:39:17 +0700

    nuke and start again

--- src/services/apiCategories.js
+++
@@ -1,9 +0,0 @@
-import supabase from "./supabase";
-
-export const getCategories = async () => {
-  const { data, error } = await supabase.from('categories').select('*');
-
-  if (error) throw new Error(error.message);
-
-  return data;
-};
